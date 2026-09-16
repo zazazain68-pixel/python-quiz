33add0c162bbb25ec3402fdc0ee0c2b822cc5c16
Zaina Muthasrik <zazazain68@gmail.com>
2026-09-16 17:22:46 +0530

    Created number guessing game

---
+++ quiz.py
@@ -0,0 +1,11 @@
+print("Welcome to Zaina's number guessing game")  
+name=input("what is your name?")
+print("Welcome",name)
+age=int(input("how old are you?"))
+print( "oh you are", age)
+print("Lets start")
+answer=int(input("guess a number"))
+if answer ==4:
+    print("yh correct guess") 
+else:
+    print("Nope better luck next time")
